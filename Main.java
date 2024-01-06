4d57310c35637c7ad9cae416ef5751f486801a2f
Nithin Bhat <Nithin.Bhat@triconinfotech.com>
2024-01-06 11:27:08 +0530

    Your commit message

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
 
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+
+class FileReadingException extends Exception {
+    public FileReadingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
 
 public class Main {
     private static final Logger logger = Logger.getLogger(Main.class.getName());
@@ -13,32 +21,35 @@ public class Main {
         String sid;
 
         do {
-            System.out.print("Enter symbol: ");
+            logger.info ("Enter symbol: ");
+
             sid = s.next();
             String line = "";
             boolean symbolFound = false;
 
-            try (FileInputStream fin = new FileInputStream("C:\\Users\\Nithin.bhat\\Desktop\\nae.txt")) {
+            try (FileInputStream fin = new FileInputStream("C:\\Users\\Nithin.bhat\\Desktop\\loggerassignment\\name.txt")) {
                 Scanner sc = new Scanner(fin);
 
+//
                 while (sc.hasNextLine()) {
                     line = sc.nextLine();
                     if (line.startsWith(sid)) {
-                        System.out.println(line);
+                        logger.info(line);
                         symbolFound = true;
                     }
                 }
-
                 if (!symbolFound) {
                     logger.log(Level.WARNING, "No line starts with that symbol: " + sid);
                 }
 
             } catch (IOException ioe) {
-                logger.log(Level.SEVERE, "Error reading file", ioe);
+
+                FileReadingException fileReadingException = new FileReadingException("Error reading file", ioe);
+                logger.log(Level.SEVERE, fileReadingException.getMessage(), fileReadingException);
             }
 
-            System.out.println("One more task? (Y/N)");
+            logger.info("One more task? (Y/N)");
+
         } while (s.next().toUpperCase().charAt(0) == 'Y');
     }
 }
-
